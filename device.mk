f31506a1d1dbfd150bd0d5859bd5db6d1b6be611
Demon000 <demonsingur@gmail.com>
2019-05-14 19:32:11 +0000

    davinci: Import lights HAL from Xiaomi msm8998-common
    Change-Id: Ieee4277f15927b4522deaec9a709809cf6db6fea

--- device.mk
+++ device.mk
@@ -98,6 +98,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.davinci
+
 # Network
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
