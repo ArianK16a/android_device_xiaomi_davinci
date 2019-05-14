1dce738c4e76c67a7ed567f5e2d4c9ee163b1d2e
Demon000 <demonsingur@gmail.com>
2019-05-14 19:32:11 +0000

    davinci: Import lights HAL from Xiaomi msm8998-common
    Change-Id: Ieee4277f15927b4522deaec9a709809cf6db6fea

--- device.mk
+++ device.mk
@@ -42,6 +42,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.davinci
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
