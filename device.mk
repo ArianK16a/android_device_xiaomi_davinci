85abcb7a35ad76a938da57ed29a0326ff0675194
Demon000 <demonsingur@gmail.com>
2019-08-11 19:27:41 +0200

    davinci: Build media stack
    Change-Id: I555f845a389c1cb5a6aef6578343d94e049e212d

--- device.mk
+++ device.mk
@@ -136,6 +136,18 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.davinci
 
+# Media
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
+
 # Network
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
