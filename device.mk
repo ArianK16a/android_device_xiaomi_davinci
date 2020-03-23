65e3c4616c9dcc1c25b4a1069f0da4953c232094
Arian <arian.kulmer@web.de>
2020-03-23 21:39:07 +0100

    davinci: Configure display density
    Change-Id: Id50971e3b28c68f5035c58aa61de2e475bd1b03a

--- device.mk
+++ device.mk
@@ -22,3 +22,12 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/davinci/davinci-vendor.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+TARGET_SCREEN_DENSITY := 440
