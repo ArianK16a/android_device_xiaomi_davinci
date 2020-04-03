26280835c50b642f003000441619f2b6fd23974d
Arian <arian.kulmer@web.de>
2020-04-04 01:07:18 +0200

    davinci: Build Snap
    Change-Id: I3b1ca243276045897335b1b26142f27c0f448d06

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ $(call inherit-product, vendor/xiaomi/davinci/davinci-vendor.mk)
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
