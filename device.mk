4b17a2702e89168c93d341eaf3363f12b5810bf1
Arian <arian.kulmer@web.de>
2020-04-03 19:02:17 +0200

    davinci: overlay: Set fingerprint position
    Change-Id: Ib21d79b955d7dffaa4afd66c83cc111a47ede14f

--- device.mk
+++ device.mk
@@ -29,7 +29,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
