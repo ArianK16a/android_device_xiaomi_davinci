c5ad03486986552dcd0e46f85c6995bf25a60298
Arian <arian.kulmer@web.de>
2020-04-09 13:08:47 +0200

    Revert "davinci: Set scaled brightness"
    This reverts commit 530ba726058fa6fee7b31ec7514c884d7c2d6cb7.

--- device.mk
+++ device.mk
@@ -120,8 +120,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
