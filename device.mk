063a7c365d66a1410032973d85b1d49a3f3ab266
Arian <arian.kulmer@web.de>
2020-04-10 15:03:59 +0200

    davinci: Build health hal
    Change-Id: I93c025a9d60e46a3239052ba2eab96d8e470a765

--- device.mk
+++ device.mk
@@ -143,6 +143,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.davinci
