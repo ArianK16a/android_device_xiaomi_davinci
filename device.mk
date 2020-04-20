a5f1c9bbc0eb48fec981073f4d81ee3afc67152b
Arian <arian.kulmer@web.de>
2020-04-21 01:05:15 +0200

    davinci: display: Copy hardware permissions from frameworks/native
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: If3e470b7d879c8b160a93655292c76baf4c1460d

--- device.mk
+++ device.mk
@@ -125,6 +125,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.davinci
