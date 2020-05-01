bf57f48bb69bb8bb301e70ee3c5e055a9f1b9918
Arian <arian.kulmer@web.de>
2020-05-01 13:36:04 +0200

    davinci: Add init.recovery.qcom.rc
    Change-Id: Ib6904420afbb15f81b6a238225f75165d1f09257

--- device.mk
+++ device.mk
@@ -295,6 +295,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
 
+PRODUCT_PACKAGES += \
+    init.recovery.qcom.rc
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
