17796480e768e1a475229c25fdfa5e7dc693a227
Arian <arian.kulmer@web.de>
2020-06-17 23:50:13 +0200

    davinci: rootdir: Increase scaling min frequency to 576MHz for little cluster
    Change-Id: I9de020adfa64b68ff617fb594446af48bb2d5ddb

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -113,6 +113,9 @@ case "$target" in
     echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
     echo 1248000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
 
+    # configure scaling min frequency for little cluster
+    echo 576000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+
     # configure governor settings for big cluster
     echo "schedutil" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
     echo 0 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/up_rate_limit_us
