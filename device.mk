2e1bcd27a8476866ffea9cdae6eafbb850eca97c
Arian <arian.kulmer@web.de>
2020-06-21 20:37:29 +0200

    davinci: Drop trust, livedisplay and lineage power
    Change-Id: I4e122a5c8c908b895747cf1a3693b95f92162105

--- device.mk
+++ device.mk
@@ -195,10 +195,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.davinci
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
@@ -360,10 +356,6 @@ PRODUCT_PACKAGES += \
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # Uevent
 PRODUCT_COPY_FILES += \
