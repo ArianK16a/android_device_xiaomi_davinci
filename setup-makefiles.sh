5e0b47626abdceed3e6f200c0f21c08a50cdb5d8
Arian <arian.kulmer@web.de>
2020-06-22 14:36:23 +0200

    davinci: Adapt extract_utils scripts to PA
    Change-Id: I369d9e11bf342804a5cb6c4d0007c48642a04c5b

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,9 +25,9 @@ INITIAL_COPYRIGHT_YEAR=2020
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ROOT}/vendor/pa/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -35,7 +35,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false
+setup_vendor "${DEVICE}" "${VENDOR}" "${ROOT}" false
 
 # Copyright headers and guards
 write_headers "${DEVICE}"
