456ba4f5970f66c9f00033663c796ea3c8c08026
Arian <arian.kulmer@web.de>
2020-08-30 23:54:04 +0200

    davinci: parts: Adjust for new led labels in kernel
    Change-Id: I8ce322f58d2af6fee20f1a9eda91e68b0ec59b05

--- parts/src/org/lineageos/settings/popupcamera/Constants.java
+++ parts/src/org/lineageos/settings/popupcamera/Constants.java
@@ -32,9 +32,9 @@ public class Constants {
     public static final String OPEN_CAMERA_STATE = "1";
 
     public static final String FRONT_CAMERA_ID = "1";
-    public static final String BLUE_LED_PATH = "/sys/class/leds/blue/brightness";
-    public static final String BREATH_BLUE_LED_PATH = "/sys/class/leds/blue/breath";
-    public static final String BREATH_GREEN_LED_PATH = "/sys/class/leds/green/breath";
-    public static final String GREEN_LED_PATH = "/sys/class/leds/green/brightness";
+    public static final String LEFT_LED_PATH = "/sys/class/leds/left/brightness";
+    public static final String BREATH_LEFT_LED_PATH = "/sys/class/leds/left/breath";
+    public static final String BREATH_RIGHT_LED_PATH = "/sys/class/leds/right/breath";
+    public static final String RIGHT_LED_PATH = "/sys/class/leds/right/brightness";
     public static final String POPUP_SOUND_PATH = "/system/media/audio/ui/";
 }
