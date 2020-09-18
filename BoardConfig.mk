d38bd3cad3bb9891694666b63da1efbd650dd1ab
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    davinci: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,9 +186,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # Security patch level
 VENDOR_SECURITY_PATCH := 2020-07-01
 
-# Sepolicy
+# Selinux
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
