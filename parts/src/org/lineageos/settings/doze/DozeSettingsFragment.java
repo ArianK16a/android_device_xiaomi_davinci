93f066591ee011053c1f8fe50e4713257a5faf0c
Arian <arian.kulmer@web.de>
2020-10-14 17:30:45 +0200

    Revert "davinci: parts: Disable raise to wake toggle when AOD is on"
    This reverts commit 264708ce3fb5d4f318bf95fc9277454fc0c9f135.

--- parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -81,9 +81,6 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
         PreferenceCategory proximitySensorCategory = (PreferenceCategory) getPreferenceScreen().
                 findPreference(DozeUtils.CATEG_PROX_SENSOR);
 
-        SwitchPreference raiseToWakeGesture = (SwitchPreference) getPreferenceScreen().
-                findPreference(DozeUtils.GESTURE_RAISE_TO_WAKE);
-
         mPickUpPreference = (SwitchPreference) findPreference(DozeUtils.GESTURE_PICK_UP_KEY);
         mPickUpPreference.setEnabled(dozeEnabled);
         mPickUpPreference.setOnPreferenceChangeListener(this);
@@ -112,7 +109,6 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
             mPickUpPreference.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
             pickupSensorCategory.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
             proximitySensorCategory.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
-            raiseToWakeGesture.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
         }
     }
 
