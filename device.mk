f203c8ab6d7ce992570896b0a8f0e22ae84a80dc
Arian <arian.kulmer@web.de>
2020-11-23 18:13:25 +0100

    Revert "davinci: btcommon: Move qti specific modules under qti flag"
    This reverts commit d2dcd358d0103dc052d6459916e2671a9baad46f.

--- device.mk
+++ device.mk
@@ -98,8 +98,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
-    vendor/qcom/opensource/commonsys/system/bt/conf
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
