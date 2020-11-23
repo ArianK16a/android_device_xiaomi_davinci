36bdfdc5fb8319b5891c04c859abf1bb5f3d0d66
Arian <arian.kulmer@web.de>
2020-11-23 18:13:32 +0100

    Revert "davinci: Bluetooth: select the appropriate Bluetooth implementation"
    This reverts commit b5dcf8b4fb0cfc1323dffa20e442a50e9ba0c19d.

--- device.mk
+++ device.mk
@@ -97,9 +97,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
