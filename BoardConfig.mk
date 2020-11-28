6f3d3a118494f7b4cf31cb24a6e9bf7bce2c27e0
Arian <arian.kulmer@web.de>
2020-11-28 18:49:15 +0100

    davinci: Don't explicitly include qcom sepolicy
    Change-Id: I9cf2081062f4412f602a42d5e9e225422868e83f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,7 +178,6 @@ VENDOR_SECURITY_PATCH := 2020-10-01
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
