95e9344e8b9aaadca66b1f3b2b9e4dfc66345ef5
Arian <arian.kulmer@web.de>
2020-12-24 16:25:21 +0100

    Revert "davinci: Drop QC value-add"
    This reverts commit 58093dd7abbb347d3c7e7f06f13d7e3fcf66fe7d.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,6 +157,7 @@ TARGET_BOARD_PLATFORM := sm6150
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
